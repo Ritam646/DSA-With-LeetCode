20a66858461734da8e4851685cfc264ef3d47fa3
Ritam Patra <146725244+Ritam646@users.noreply.github.com>
2025-12-17 22:09:50 +0530

    Time: 3 ms (40.33%) | Memory: 43 MB (52.23%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,13 +1,17 @@
+import java.util.Arrays;
 class Solution {
     public char findTheDifference(String s, String t) {
-        char result = 0;
-        for (char c : s.toCharArray()) {
-            result ^= c;
-        }
-        for (char c : t.toCharArray()) {
-            result ^= c;
-        }
-        return result;
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
 
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                return b[i];
+            }
+        }
+
+        return b[b.length - 1];
     }
 }
