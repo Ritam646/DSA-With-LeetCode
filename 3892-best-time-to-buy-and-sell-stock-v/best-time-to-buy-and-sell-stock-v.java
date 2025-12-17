d18fb483d8fd602f85834342b992e652ebf91a49
Ritam Patra <146725244+Ritam646@users.noreply.github.com>
2025-12-17 22:33:08 +0530

    Time: 13 ms (95.39%) | Memory: 46.4 MB (86.17%) - LeetSync

---
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long maximumProfit(int[] prices, int k) {
+        long NEG = Long.MIN_VALUE / 4;
+        long[][] dp = new long[k + 1][3];
+        for (int t = 0; t <= k; t++) {
+            dp[t][0] = 0;
+            dp[t][1] = NEG;
+            dp[t][2] = NEG;
+        }
+        for (int price : prices) {
+            for (int t = k; t >= 1; t--) {
+                dp[t][0] = Math.max(
+                        dp[t][0],
+                        Math.max(dp[t][1] + price, dp[t][2] - price)
+                );
+                dp[t][1] = Math.max(dp[t][1], dp[t - 1][0] - price);
+                dp[t][2] = Math.max(dp[t][2], dp[t - 1][0] + price);
+            }
+        }
+        return dp[k][0];
+    }
+}
